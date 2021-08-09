c0647663f47d61233b83d2ffc3a30eb5c9261a86
abdalafo98 <abdalafo.abubaker@gmail.com>
2021-08-09 09:25:45 +0300

    add getToFavorite

--- routers/controllers/favorite.js
+++ routers/controllers/favorite.js
@@ -1,6 +1,6 @@
 const favoriteModel = require("./../../db/model/favirote");
 
-const addToFavorite = (req, res) => {
+const addToFavoriteById = (req, res) => {
   const _id = req.token._id;
   const { productId } = req.body;
 
@@ -14,4 +14,30 @@ const addToFavorite = (req, res) => {
     });
 };
 
-module.exports = { addToFavorite };
+const getFavoriteById = (req, res) => {
+  const _id = req.token._id;
+
+  favoriteModel
+    .findOne({ user: _id })
+    .populate("Product")
+    .exec()
+    .then((result) => {
+      res.status(200).json(result);
+    })
+    .catch((err) => {
+      res.status(400).json(err);
+    });
+};
+const deleteFavoriteById = (req, res) => {
+  const _id = req.token._id;
+  const { productId } = req.body;
+  favoriteModel
+    .updateOne({ user: _id }, { $pull: { products: productId } })
+    .then((result) => {
+      res.status(200).json(result);
+    })
+    .catch((err) => {
+      res.status(400).json(err);
+    });
+};
+module.exports = { addToFavoriteById, getFavoriteById, deleteFavoriteById };
